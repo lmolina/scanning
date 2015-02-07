369a94962456fc564db720cb141c6732c6abdace
Laudin Alessandro Molina T <laudin.molina@gmail.com>
2015-02-07 01:08:10 +0100

    Reimplementing the scan

--- scan.h
+++ scan.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 #include <cstdio>
@@ -11,26 +12,101 @@
 
 #include <sqlite3.h>
 
+struct ProbeResponse {
+  int op_channel;
+  int nic_channel;
+  int delay;
+  int jiffies;
+  float kernel_time;
+  std::string ssid;
+  std::string bssid;
+  std::string type;
+};
 
-class Scan {
+static bool cmp(const ProbeResponse & a, const ProbeResponse & b) {
+  return a.delay < b.delay;
+}
+
+class Channel {
   public:
-    struct AP {
-      int channel;
-      int delay;
-      std::string ssid;
-      std::string bssid;
-      std::string type;
-    };
-    
+    Channel(int id=0) {
+      this->id = id;
+    }
 
-    typedef std::vector<AP> ScanResults;
+    void addResponse(ProbeResponse & presp) {
+      probe_responses.push_back(presp);
+    }
 
-    Scan(std::string dbName, std::string experiment) {
+    int size() {
+      return probe_responses.size();
+    }
+
+    std::vector<ProbeResponse> responses() {
+      return probe_responses;
+    }
+
+  private:
+    int id;
+    std::vector<ProbeResponse> probe_responses;
+};
+
+
+class ScanResult {
+  public:
+    //ScanResult();
+
+
+    // addResponse will save the ProbeResponse in the scan_id scan and in
+    // the appropriate channel, using nic_channel
+    void addResponse(ProbeResponse & presp) {
+      int ch = presp.nic_channel;
+
+      // Check if the channel already exists in the map, create the channel
+      // if necessary
+      if (results.count(ch) == 0) {
+        // results[channel_id] = new Channel(channel_id);
+        results[ch] = Channel(ch);
+      }
+
+      results[ch].addResponse(presp);
+    }
+
+
+    Channel channel(int ch) {
+      Channel r;
+      if (results.count(ch) == 0) {
+        return r;
+      }
+
+      return results[ch];
+    }
+
+  private:
+
+    // results group the ProbeResponses registered at each channel. Since we
+    // don't know what are the scanned channels, we use a map to holds it
+    std::map<int, Channel> results;
+};
+
+
+class ScanningCampaing {
+  public:
+    typedef std::vector<ProbeResponse> ScanResults;
+
+    ScanningCampaing(std::string dbName, std::string experiment) {
       this->dbName = dbName;
       this->experiment = experiment;
       this->rand = NULL;
       this->gen = NULL;
     };
+
+    ~ScanningCampaing() {
+      if (rand != NULL)
+        delete rand;
+
+      if (gen != NULL)
+        delete gen;
+    }
 
     void init() {
       sqlite3* db;
@@ -44,13 +120,13 @@ class Scan {
       fprintf(stdout, "Database opened successfully :)\n");
 
       std::string sql;
-      sql = "SELECT algorithm, scan, op_channel, ssid, bssid, delayj, frame_type ";
+      sql = "SELECT algorithm, scan, op_channel, nic_channel, jiffies, kernel_time, ssid, bssid, delayj, frame_type ";
       sql += " FROM frames ";
       sql += " WHERE algorithm = \"" + experiment + "\";";
 
       // A trick to modify the scans in the callback function (because it's
       // an static function)
-      std::vector<ScanResults>* data = &scans;
+      std::vector<ScanResult>* data = &scans;
       char * errMsg = 0;
       rc = sqlite3_exec(db, sql.c_str(), callback, (void*)data, &errMsg);
 
@@ -63,7 +139,8 @@ class Scan {
       N = scans.size();
 
       for(auto & i: scans) {
-        std::sort(i.begin(), i.end(), cmp);
+        // TODO: check the cmp operator
+        //std::sort(i.begin(), i.end(), cmp);
       }
 
       // Prepare the random generator
@@ -72,47 +149,83 @@ class Scan {
       rand = new std::uniform_int_distribution<>(0, scans.size() - 1);
     }
 
-    std::vector<AP> execute(int ch, int minCT, int maxCT) {
-      std::vector<AP> results;
+
+    /*
+     * Emulate a channel probe using the corresponding parameters (Channel,
+     * MinChannelTime and MaxChannelTime.
+     *
+     * The emulation works as follows:
+     * 1) An scan result is randomly selected from the database
+     *
+     * 2) If there is at least one ProbeResponse with a response delay <=
+     *    MinChannelTime, then look loop over the ProbeResponses and return
+     *    all of those with a response delay <= MaxChannelTime
+     *
+     * 3) If there is no ProbeResponse at all, then return an empty vector
+     *
+     * 4) If there are several ProbeResponses but all of them contains the
+     *    response delay greater than MinChannelTime, then return an empty
+     *    vector
+     */
+    //std::vector<ProbeResponse> execute(int ch, int min_ct, int max_ct) {
+    ScanResults randomScan(int ch, int min_ct, int max_ct) {
+      ScanResult scan;
+      Channel channel;
+      std::vector<ProbeResponse> all_responses;
+      ScanResults r;
 
       //unsigned int scanId = rand() % N;
-      unsigned int scanId = (*rand)(*gen);
+      //unsigned int scan_id = (*rand)(*gen);
+      unsigned int scan_id = 10;
+      scan = scans[scan_id];
 
-      if (scans[scanId].size() == 0)
-        return results;
-
-      if (minCT < scans[scanId][0].delay)
-        return results;
-
-      for (int i = 0; i < scans[scanId].size(); ++i) {
-        if (scans[scanId][i].delay <= maxCT)
-          results.push_back(scans[scanId][i]);
-        else
-          return results;
+      channel = scan.channel(ch);
+      if (channel.size() <= 0) {
+        return r;
       }
 
-      return results;
+      all_responses = channel.responses();
+      std::sort(all_responses.begin(), all_responses.end(), cmp);
+
+      if (min_ct < all_responses[0].delay)
+        return r;
+
+      for (int i = 0; i < all_responses.size(); ++i) {
+        if (all_responses[i].delay <= max_ct)
+          r.push_back(all_responses[i]);
+        else
+          return r;
+      }
+
+      return r;
     }
 
-    ~Scan() {
-      if (rand != NULL)
-        delete rand;
-
-      if (gen != NULL)
-        delete gen;
-    }
 
   private:
     static int callback(void* data, int argc, char **argv, char **colName) {
-      AP buffer;
+      ProbeResponse buffer;
+      
+      std::vector<ScanResult>* tmp = (std::vector<ScanResult>*) data;
 
-      unsigned int scanId = 0;
+      unsigned int scan_id = 0;
       for (int i = 0; i < argc; ++i) {
         if (strcmp(colName[i], "scan") == 0) {
-          scanId = atoi(argv[i]) - 1;
+          
+          // The "scan" column starts at 1, so scan - 1 to use it as 
+          // the vector index in the results
+          scan_id = atoi(argv[i]) - 1;
         }
         else if (strcmp(colName[i], "op_channel") == 0) {
-          buffer.channel = atoi(argv[i]);
+          buffer.op_channel = atoi(argv[i]);
+        }
+        else if (strcmp(colName[i], "nic_channel") == 0) {
+          buffer.nic_channel = atoi(argv[i]);
+        }
+        else if (strcmp(colName[i], "kernel_time") == 0) {
+          buffer.kernel_time = atof(argv[i]);
+        }
+        else if (strcmp(colName[i], "jiffies") == 0) {
+          buffer.jiffies = atoi(argv[i]);
         }
         else if (strcmp(colName[i], "delayj") == 0) {
           buffer.delay = atoi(argv[i]);
@@ -128,24 +241,29 @@ class Scan {
         }
       }
 
-      std::vector<ScanResults>* tmp = (std::vector<ScanResults>*) data;
-      if (scanId >= tmp->size()) {
-        tmp->resize(scanId + 1);
+
+      // Remember that the scan_id is the index of the scan in the vector of
+      // scan results (that is a vector of ProbeResponses).
+      if (scan_id >= tmp->size()) {
+        tmp->resize(scan_id + 1);
       }
 
-      (tmp->at(scanId)).push_back(buffer);
+      (tmp->at(scan_id)).addResponse(buffer);
 
       return 0;
     }
 
-    static bool cmp(const AP & a, const AP & b) {
-      return a.delay < b.delay;
-    }
+
+
+    // One ScanResult contains the set of results obtained when a real
+    // scanning was triggered in a real machine, this could be related to a
+    // fixed location, although, this might not be true if the MS is in
+    // movement. So, the results holds each one of the ScanResult
+    std::vector<ScanResult> scans;
 
     unsigned int N;
     std::string dbName;
     std::string experiment;
-    std::vector<ScanResults> scans;
     std::uniform_int_distribution<>* rand;
     std::mt19937* gen;
 };
--- test_scan.cpp
+++ test_scan.cpp
@@ -12,18 +12,22 @@ int main(int argc, char ** argv) {
   }
 
   //Scan scan("/home/lmolina/src/wifidb/test_18.1/database.db");
-  Scan scan(argv[1], argv[2]);
+  ScanningCampaing scan(argv[1], argv[2]);
   scan.init();
 
-  srand(14);
-  Scan::ScanResults results = scan.execute(11, 10, 30);
+  //srand(14);
+  ScanningCampaing::ScanResults results = scan.randomScan(11, 10, 30);
   std::cout << results.size() << " results: " << std::endl;
 
   for (auto & r: results) {
+    printf("kernel_time: %.9f\n", r.kernel_time);
+    printf("jiffies: %d\n", r.jiffies);
     std::cout << "type: " << r.type << std::endl;
     std::cout << "bssid: " << r.bssid << std::endl;
     std::cout << "ssid: " << r.ssid << std::endl;
     std::cout << "delay: " << r.delay << std::endl;
+    std::cout << "op_channel: " << r.op_channel << std::endl;
+    std::cout << "nic_channel: " << r.nic_channel << std::endl;
     std::cout << std::endl;
   }
 
